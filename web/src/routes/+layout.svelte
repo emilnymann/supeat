7e3a85df232688f5ea294c1c0b0b24b611aa655d
Emil Nymann Sølyst <emilnymann96@gmail.com>
2024-06-29 09:09:27 +0200

    feat: use google material symbols and noto font

---
+++ web/src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wdth,wght@0,62.5..100,100..900;1,62.5..100,100..900&display=swap"
+		rel="stylesheet"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+	/>
+</svelte:head>
+
+<style>
+	:global(body) {
+		font-family: 'Noto Sans', sans-serif;
+	}
+</style>
