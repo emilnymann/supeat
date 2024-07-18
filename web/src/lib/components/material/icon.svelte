ca4c4f6d61fcdada2913e6da255eb942ea898e90
Emil Nymann Sølyst <emilnymann96@gmail.com>
2024-07-18 23:44:22 +0200

    build(scraper): build scraper image based on alpine

--- web/src/lib/components/material/icon.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { MaterialSymbolToken } from '$lib/types/material/IconEnum';
-
-	export let token: MaterialSymbolToken;
-	export let style: 'rounded' | 'outlined' | 'sharp' = 'rounded';
-</script>
-
-<span class="material-symbols-{style}">{token}</span>
--- web/src/lib/types/material/IconEnum.ts
+++
@@ -1,6 +0,0 @@
-export enum MaterialSymbolToken {
-    DinnerDining = 'dinner_dining',
-    BreakfastDining = 'breakfast_dining',
-    EggAlt = 'egg_alt',
-    Nutrition = 'nutrition',
-}
